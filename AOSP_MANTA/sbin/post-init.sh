fdf2f84542f0e6f6a59ccc0dc2499d03bb35c527
ktoonsez <ktoonsez@gmail.com>
2013-01-03 21:10:56 -0700

    try some opto flags from EniGmA1987

--- AOSP_MANTA/sbin/post-init.sh
+++ AOSP_MANTA/sbin/post-init.sh
@@ -2,5 +2,12 @@
 
 echo 1 > /proc/sys/net/ipv4/tcp_tw_recycle;
 echo 1 > /proc/sys/net/ipv4/tcp_tw_reuse;
+echo 1024 > /proc/sys/kernel/random/read_wakeup_threshold;
+echo 2048 > /proc/sys/kernel/random/write_wakeup_threshold;
+echo 256000000 > /proc/sys/kernel/shmmax;
+echo 1024000 > /proc/sys/net/core/rmem_max;
+echo 1024000 > /proc/sys/net/core/wmem_max;
+echo 10 > /proc/sys/vm/swappiness;
+echo 20 > /proc/sys/vm/vfs_cache_pressure;
 
 echo $(date) END of post-init.sh
